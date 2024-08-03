382837a6b244de2b0c7dc2022ca31009bfa126e9
Pushkar Kulkarni <72039175+pushkarkn@users.noreply.github.com>
2024-08-03 12:27:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Black-Scholes Option Pricing Model')
+st.title('Black-Scholes Pricing Model')
 
 st.info('Black Scholes option pricing model with a dynamic PNL Heatmap')
 with st.sidebar:
@@ -14,5 +14,5 @@ with st.sidebar:
     v = st.number_input('Volatility (σ)')
     r = st.number_input('Risk-Free Interest Rate')
     
-    submit = st.form_submit_button(Enter)
+    submit = st.form_submit_button()
 
