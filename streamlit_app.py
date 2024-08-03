677019b924ca52b2ae2424d48a450dd3378677c5
Pushkar Kulkarni <72039175+pushkarkn@users.noreply.github.com>
2024-08-03 12:22:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,11 @@ st.info('Black Scholes option pricing model with a dynamic PNL Heatmap')
 with st.sidebar:
   st.header('Inputs Go Here')
   with st.form('addition'):
-    a = st.number_input('a')
-    b = st.number_input('b')
+    cap = st.number_input('Current Asset Price')
+    sp = st.number_input('Strike Price')
+    t = st.number_input('Time To Maturity (Years)')
+    v = st.number_input('Volatility (σ)')
+    r = st.number_input('Risk-Free Interest Rate')
+    
+    submit = st.form_submit_button()
 
