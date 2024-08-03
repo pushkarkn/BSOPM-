ca51269e839b8ec452823f7b0985d928f52370a7
Pushkar Kulkarni <72039175+pushkarkn@users.noreply.github.com>
2024-08-03 11:44:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title('Option Pricing App')
 
 st.info('Black Scholes option pricing model with a dynamic PNL Heatmap')
-
+with st.sidebar:
+  st.header('Inputs Go Here')
